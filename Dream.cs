995670b2ed36a9d0ee5643856824f590281b0d96
Tom <flam3@ymail.com>
2025-10-27 15:51:58 +0000

    Add files via upload

---
+++ Dream.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dream", menuName = "DreamsScriptableObjects/Dream", order = 1)]
+public class Dream : ScriptableObject
+{
+    [SerializeField] public string sceneName;
+    [SerializeField] public List<DreamCondition> conditions;
+}
---
+++ Dreams.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dreams : MonoBehaviour
+{
+    [Header("Dreams")]
+    [SerializeField] private List<Dream> dreams = new List<Dream>();
+
+    private List<Dream> EvaluateDreams()
+    {
+        List<Dream> validDreams = new List<Dream>();
+        foreach (Dream dream in dreams)
+        {
+            bool conditionsMet = true;
+            foreach (DreamCondition condition in dream.conditions)
+            {
+                if (!condition.Evaluate())
+                {
+                    conditionsMet = false;
+                }
+            }
+
+            if (conditionsMet)
+            {
+                validDreams.Add(dream);
+            }
+        }
+        
+        return validDreams;
+    }
+
+    public void Dream()
+    {
+        List<Dream> validDreams = EvaluateDreams();
+
+        int dreamsCount = validDreams.Count;
+        if (dreamsCount > 0)
+        {
+            int randomDreamIndex = Random.Range(0, dreamsCount);
+
+            Dream randomDream = validDreams[randomDreamIndex];
+
+            dreams.Remove(randomDream);
+
+            SceneManager.LoadScene(randomDream.sceneName);
+        }
+    }
+}
